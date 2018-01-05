ee93771e28be8b65fcdd77c9760b59f6cf1c1cbd
J.W. Prescott <jwprescott@gmail.com>
2018-01-05 11:10:31 -0500

    Rudimentary working interface for relabeling images

--- annotate_cxr8.py
+++ annotate_cxr8.py
@@ -4,7 +4,8 @@
 import os
 from PIL import Image
 import matplotlib.pyplot as plt
-from matplotlib.widgets import RadioButtons
+import matplotlib.gridspec as gridspec
+from matplotlib.widgets import RadioButtons, Button
 import time
 
 data_dir = '/home/ubuntu/Desktop/torch-cxr8/images_pneumonia_vs_negative/train/1_images_infection'
@@ -17,93 +18,68 @@ label_file = '/home/ubuntu/Desktop/torch-cxr8/relabeled.csv'
 # 3: maybe pneumonia (atelectasis, effusion, combination, etc)
 
 files = os.listdir(data_dir)
+dx = ('Pneumonia','Not pneumonia')
+dxdict = dict.fromkeys(files)
+
 img = Image.open(os.path.join(data_dir,files[0]))
-#for file in files[0:1]:
-##for file in os.listdir(data_infection_dir):
-    #img = Image.open(os.path.join(data_dir,file))
-    ##plt.axis('off')
-    #plt.show()
     
-fig, ax = plt.subplots()
-#l, = ax.plot(t, s0, lw=2, color='red')
-l, = ax.imshow(img)
-plt.subplots_adjust(left=0.3)
+fig = plt.figure(figsize=(15,15))
+plt.axis('off')
+l = plt.imshow(img)
 
 axcolor = 'lightgoldenrodyellow'
-rax = plt.axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(rax, ('2 Hz', '4 Hz', '8 Hz'))
+rax = plt.axes([0.02, 0.7, 0.15, 0.15], facecolor=axcolor)
+radio = RadioButtons(rax, dx)
 
 
 def hzfunc(label):
-    hzdict = {'2 Hz': s0, '4 Hz': s1, '8 Hz': s2}
+    hzlist = ('Pneumonia','Not pneumonia')
     #ydata = hzdict[label]
     #l.set_ydata(ydata)
     #plt.draw()
 radio.on_clicked(hzfunc)
 
-#rax = plt.axes([0.05, 0.4, 0.15, 0.15], facecolor=axcolor)
-#radio2 = RadioButtons(rax, ('red', 'blue', 'green'))
+class Index(object):
+    ind = 0
 
+    def next(self, event):
+        currDx = radio.value_selected
+        currFile = files[self.ind]
+        dxdict[currFile] = currDx
+        self.ind += 1
+        newFile = files[self.ind]
+        newDx = dxdict[newFile]
+        img = Image.open(os.path.join(data_dir,newFile))
+        l.set_data(img)
+        if newDx == None:
+            radio.set_active(0)
+        else:
+            radio.set_active(dx.index(newDx))
+        plt.title(files[self.ind])
+        plt.draw()
 
-#def colorfunc(label):
-    #l.set_color(label)
-    #plt.draw()
-#radio2.on_clicked(colorfunc)
+    def prev(self, event):
+        currDx = radio.value_selected
+        currFile = files[self.ind]
+        dxdict[currFile] = currDx
+        self.ind -= 1
+        newFile = files[self.ind]
+        newDx = dxdict[newFile]
+        img = Image.open(os.path.join(data_dir,newFile))
+        l.set_data(img)
+        if newDx == None:
+            radio.set_active(0)
+        else:
+            radio.set_active(dx.index(newDx))
+        plt.title(files[self.ind])
+        plt.draw()
 
-#rax = plt.axes([0.05, 0.1, 0.15, 0.15], facecolor=axcolor)
-#radio3 = RadioButtons(rax, ('-', '--', '-.', 'steps', ':'))
-
-
-#def stylefunc(label):
-    #l.set_linestyle(label)
-    #plt.draw()
-#radio3.on_clicked(stylefunc)
+callback = Index()
+axprev = plt.axes([0.02, 0.6, 0.1, 0.075])
+axnext = plt.axes([0.13, 0.6, 0.1, 0.075])
+bnext = Button(axnext, 'Next')
+bnext.on_clicked(callback.next)
+bprev = Button(axprev, 'Previous')
+bprev.on_clicked(callback.prev)
 
 plt.show()
-
-
-
-#import numpy as np
-#import matplotlib.pyplot as plt
-#from matplotlib.widgets import RadioButtons
-
-#t = np.arange(0.0, 2.0, 0.01)
-#s0 = np.sin(2*np.pi*t)
-#s1 = np.sin(4*np.pi*t)
-#s2 = np.sin(8*np.pi*t)
-
-#fig, ax = plt.subplots()
-#l, = ax.plot(t, s0, lw=2, color='red')
-#plt.subplots_adjust(left=0.3)
-
-#axcolor = 'lightgoldenrodyellow'
-#rax = plt.axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
-#radio = RadioButtons(rax, ('2 Hz', '4 Hz', '8 Hz'))
-
-
-#def hzfunc(label):
-    #hzdict = {'2 Hz': s0, '4 Hz': s1, '8 Hz': s2}
-    #ydata = hzdict[label]
-    #l.set_ydata(ydata)
-    #plt.draw()
-#radio.on_clicked(hzfunc)
-
-#rax = plt.axes([0.05, 0.4, 0.15, 0.15], facecolor=axcolor)
-#radio2 = RadioButtons(rax, ('red', 'blue', 'green'))
-
-
-#def colorfunc(label):
-    #l.set_color(label)
-    #plt.draw()
-#radio2.on_clicked(colorfunc)
-
-#rax = plt.axes([0.05, 0.1, 0.15, 0.15], facecolor=axcolor)
-#radio3 = RadioButtons(rax, ('-', '--', '-.', 'steps', ':'))
-
-
-#def stylefunc(label):
-    #l.set_linestyle(label)
-    #plt.draw()
-#radio3.on_clicked(stylefunc)
-
-#plt.show()
