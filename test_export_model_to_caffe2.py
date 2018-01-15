511c8cdfc316e7d8d2d9a5f8da53a1bee029e95d
J.W. Prescott <jwprescott@gmail.com>
2018-01-15 11:31:37 -0800

    Successful test of densenet121 with default ImageNet parameters

--- test_export_model_to_caffe2.py
+++ test_export_model_to_caffe2.py
@@ -7,10 +7,14 @@ import numpy as np
 from torch import nn
 from torch.autograd import Variable
 import torch.utils.model_zoo as model_zoo
+from torchvision import datasets, models, transforms
 import torch.onnx
 
 # Super Resolution model definition in PyTorch
 import torch.nn.init as init
+
+from PIL import Image
+
 
 class SuperResolutionNet(nn.Module):
     def __init__(self, upscale_factor, inplace=False):
@@ -38,36 +42,63 @@ class SuperResolutionNet(nn.Module):
         init.orthogonal(self.conv3.weight, init.calculate_gain('relu'))
         init.orthogonal(self.conv4.weight)
 
-# Create the super-resolution model by using the above model definition.
-torch_model = SuperResolutionNet(upscale_factor=3)
 
-# Load pretrained model weights
-model_url = 'https://s3.amazonaws.com/pytorch/test_data/export/superres_epoch100-44c6958e.pth'
-batch_size = 1    # just a random number
+## Create the super-resolution model by using the above model definition.
+#torch_model = SuperResolutionNet(upscale_factor=3)
 
-# Initialize model with the pretrained weights
-map_location = lambda storage, loc: storage
-if torch.cuda.is_available():
-    map_location = None
-torch_model.load_state_dict(model_zoo.load_url(model_url, map_location=map_location))
+## Load pretrained model weights
+#model_url = 'https://s3.amazonaws.com/pytorch/test_data/export/superres_epoch100-44c6958e.pth'
 
-# set the train mode to false since we will only run the forward pass.
+## Initialize model with the pretrained weights
+#map_location = lambda storage, loc: storage
+#if torch.cuda.is_available():
+    #map_location = None
+#torch_model.load_state_dict(model_zoo.load_url(model_url, map_location=map_location))
+
+## set the train mode to false since we will only run the forward pass.
+#torch_model.train(False)
+
+## Input to the model
+#batch_size = 1    # just a random number
+#x = Variable(torch.randn(batch_size, 1, 224, 224), requires_grad=True)
+
+## Export the model
+#torch_out = torch.onnx._export(torch_model,             # model being run
+                               #x,                       # model input (or a tuple for multiple inputs)
+                               #"super_resolution.onnx", # where to save the model (can be a file or file-like object)
+                               #export_params=True)      # store the trained parameter weights inside the model file
+
+# Try densenet model
+torch_model = models.densenet121(pretrained=True)
+
 torch_model.train(False)
 
 # Input to the model
-x = Variable(torch.randn(batch_size, 1, 224, 224), requires_grad=True)
+preprocess = transforms.Compose([
+   transforms.Resize((224,224)),
+   transforms.ToTensor(),
+   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
-# Export the model
+img_pil = Image.open('/home/prescott/Projects/tf-hemorrhage/images_curated/subarachnoid_hemorrhage/34TY8_0_0012.jpg')
+
+img_tensor = preprocess(img_pil)
+x = Variable(img_tensor.unsqueeze(0))
+    
+#batch_size = 1    # just a random number
+#x = Variable(torch.randn(batch_size, 3, 224, 224), requires_grad=True)
+
 torch_out = torch.onnx._export(torch_model,             # model being run
                                x,                       # model input (or a tuple for multiple inputs)
-                               "super_resolution.onnx", # where to save the model (can be a file or file-like object)
+                               "densenet121.onnx", # where to save the model (can be a file or file-like object)
                                export_params=True)      # store the trained parameter weights inside the model file
 
 import onnx
 import onnx_caffe2.backend
 
 # Load the ONNX ModelProto object. model is a standard Python protobuf object
-model = onnx.load("super_resolution.onnx")
+#model = onnx.load("super_resolution.onnx")
+model = onnx.load("densenet121.onnx")
 
 # prepare the caffe2 backend for executing the model this converts the ONNX model into a
 # Caffe2 NetDef that can execute it. Other ONNX backends, like one for CNTK will be
@@ -102,11 +133,27 @@ from caffe2.python.predictor import mobile_exporter
 c2_net = core.Net(c2_model)
 init_net, predict_net = mobile_exporter.Export(c2_workspace, c2_net, c2_model.external_input)
 
+##Another possible way of doing the previous steps without using a workspace (from facebook's AICamera example ipython notebook, https://caffe2.ai/docs/AI-Camera-demo-android.html)
+#from onnx_caffe2.backend import Caffe2Backend as c2
+#init_net, predict_net = c2.onnx_graph_to_caffe2_net(model.graph, True)
+
 # Let's also save the init_net and predict_net to a file that we will later use for running them on mobile
 with open('init_net.pb', "wb") as fopen:
     fopen.write(init_net.SerializeToString())
 with open('predict_net.pb', "wb") as fopen:
     fopen.write(predict_net.SerializeToString())
+
+## Verify it runs with predictor (from facebook's AICamera example)
+#with open("squeeze_init_net.pb") as f:
+    #init_net = f.read()
+#with open("squeeze_predict_net.pb") as f:
+    #predict_net = f.read()
+#from caffe2.python import workspace
+#p = workspace.Predictor(init_net, predict_net)
+## The following code should run:
+## img = np.random.rand(1, 3, 224, 224).astype(np.float32)
+## p.run([img])
+
     
 # Some standard imports
 from caffe2.proto import caffe2_pb2
@@ -120,10 +167,17 @@ from matplotlib import pyplot
 from skimage import io, transform
 from IPython import display
 
-# load the cat image and convert it to Ybr format
+## load the cat image and convert it to Ybr format
 img = Image.open("/home/prescott/Projects/Pytorch_fine_tuning_Tutorial/cat_224x224.jpg")
 img_ycbcr = img.convert('YCbCr')
 img_y, img_cb, img_cr = img_ycbcr.split()
+
+# load the hemorrhage image
+img = Image.open('/home/prescott/Projects/tf-hemorrhage/images_curated/subarachnoid_hemorrhage/34TY8_0_0012.jpg')
+img_tensor = preprocess(img)
+#img_y = img_tensor.cpu()
+img_y = img_tensor.unsqueeze(0).cpu()
+
 
 # Let's run the mobile nets that we generated above so that caffe2 workspace is properly initialized
 workspace.RunNetOnce(init_net)
@@ -137,22 +191,24 @@ print(net_printer.to_string(core.Net(predict_net)))
 #display.Image(graph.create_png(), width=800)
 
 # Now, let's also pass in the resized cat image for processing by the model.
-workspace.FeedBlob("1", np.array(img_y)[np.newaxis, np.newaxis, :, :].astype(np.float32))
+#workspace.FeedBlob("1", np.array(img_y)[np.newaxis, np.newaxis, :, :].astype(np.float32))
+workspace.FeedBlob("1", np.array(img_y).astype(np.float32))
 
 # run the predict_net to get the model output
 workspace.RunNetOnce(predict_net)
 
 # Now let's get the model output blob
-img_out = workspace.FetchBlob("27")
+#img_out = workspace.FetchBlob("27")
+img_out = workspace.FetchBlob("1274")
 
 img_out_y = Image.fromarray(np.uint8((img_out[0, 0]).clip(0, 255)), mode='L')
 
-# get the output image follow post-processing step from PyTorch implementation
-final_img = Image.merge(
-    "YCbCr", [
-        img_out_y,
-        img_cb.resize(img_out_y.size, Image.BICUBIC),
-        img_cr.resize(img_out_y.size, Image.BICUBIC),
-    ]).convert("RGB")
+## get the output image follow post-processing step from PyTorch implementation
+#final_img = Image.merge(
+    #"YCbCr", [
+        #img_out_y,
+        #img_cb.resize(img_out_y.size, Image.BICUBIC),
+        #img_cr.resize(img_out_y.size, Image.BICUBIC),
+    #]).convert("RGB")
 
-final_img.save("/home/prescott/Projects/Pytorch_fine_tuning_Tutorial/cat_superres_mobile.jpg")
+#final_img.save("/home/prescott/Projects/Pytorch_fine_tuning_Tutorial/cat_superres_mobile.jpg")
